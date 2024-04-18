b6df688bb0c4e216051e7b777a1f86168701387b
jwarner8 <james.warner@metoffice.gov.uk>
2024-04-18 15:08:45 +0100

    update docstring

--- src/CSET/operators/ageofair.py
+++ src/CSET/operators/ageofair.py
@@ -12,17 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Age of air operator, which determines how old air is since it entered through the lateral boundary at some given leadtime."""
+"""
+Age of air operator.
 
-import datetime
-import multiprocessing
+This determines how old air is since it entered through the lateral boundary at some given leadtime.
+"""
+
 import os
-from functools import partial
 from math import atan2, cos, radians, sin, sqrt
 
 import iris
 import numpy as np
-from scipy.ndimage import gaussian_filter
 
 iris.FUTURE.datum_support = True
 
@@ -32,17 +32,19 @@ def calc_dist(coord_1, coord_2):
     # Approximate radius of earth in km
     R = 6378.0
 
+    # extract coordinates and convert to radians
     lat1 = radians(coord_1[0])
     lon1 = radians(coord_1[1])
     lat2 = radians(coord_2[0])
     lon2 = radians(coord_2[1])
 
+    # Find out delta latitude, longitude
     dlon = lon2 - lon1
     dlat = lat2 - lat1
 
+    # Compute distance
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
     distance = R * c
 
     return distance * 1000
@@ -59,14 +61,14 @@ def aoa_core(
     plev_idx,
     timeunit,
     cyclic,
-    tmpdir,
     lon_pnt,
+    tmpdir,
 ):
     """
     Part of the multiprocessing capability.
 
     Requires access
-    to the full array to do the back trajectory, so there is some scaling of increase
+    to the full array to do the back trajectory, so there is some scaling of increased
     no of cores and increased mem demands. For short forecasts, or small latitude arrays,
     there is not much benefit from multicore (more overhead from IO).
 
@@ -175,137 +177,181 @@ def aoa_core(
 
 
 def compute_ageofair(
-    cubelist, plev, includevertical=True, cyclic=False, tmpdir="/tmp/", multicore=None
+    XWIND: iris.cube.Cube,
+    YWIND: iris.cube.Cube,
+    WWIND: iris.cube.Cube,
+    GEOPOT: iris.cube.Cube,
+    plev: int,
+    incW: bool,
+    cyclic: bool,
 ):
     """
-    Compute back trajectories for a particular timeslice of a forecast.
+    Compute back trajectories for a given forecast.
 
-    Arguments:
-        cubelist: an iris.CubeList object, containing 4 cubes (u, v, w component of wind, with geopotential
-                  height. All cubes 4D, structured time, pressure, latitude, longitude. Assumed correct
-                  ordering of arrays. Expects data to already be regridded onto a standard 0p5 degree grid.
-        plev: pressure level  to evaluate the back trajectory on (etc 700 or 70000) depending on hPa or Pa.
-        includevertical: If True, use vertical velocity vector to compute back trajectories. If False, then
-                         assume flow laminar (set vertical velocity to zero).
-        tmpdir: Location of a temporary directory that can be used to write intermediate files, as used
-                when multiprocessing.
-        multicore: If set as None, then run on a single thread. Otherwise, split over number of multicores.
+    This allows us to determine when air entered through the boundaries. This will run on all available
+    lead-times, and thus return an age of air cube of shape ntime, nlat, nlon.
+
+    Arguments
+    ----------
+    XWIND: iris.cube.Cube
+        An iris cube containing the x component of wind on pressure levels, on a 0p5 degree grid.
+        Requires 4 dimensions, ordered time, pressure, latitude and longitude. Must contain at
+        least 2 time points to compute back trajectory.
+    YWIND: iris.cube.Cube
+        An iris cube containing the y component of wind on pressure levels, on a 0p5 degree grid.
+        Requires 4 dimensions, ordered time, pressure, latitude and longitude. Must contain at
+        least 2 time points to compute back trajectory.
+    WWIND: iris.cube.Cube
+        An iris cube containing the w component of wind on pressure levels, on a 0p5 degree grid.
+        Requires 4 dimensions, ordered time, pressure, latitude and longitude. Must contain at
+        least 2 time points to compute back trajectory.
+    GEOPOT: iris.cube.Cube
+        An iris cube containing geopotential height on pressure levels, on a 0p5 degree grid.
+        Requires 4 dimensions, ordered time, pressure, latitude and longitude. Must contain at
+        least 2 time points to compute back trajectory.
+    plev: int
+        The pressure level of which to compute the back trajectory on. The function will search to
+        see if this exists and if not, will raise an exception.
+    incW: bool
+        If incW is True, then the back trajectories will take into account vertical velocity fields
+        and thus a back trajectory takes a 3D path back. In this case, vertical wind fields are
+        smoothed in addition to the prior coarsening, to prevent instantaneous sampling of strong
+        updrafts/downdrafts that would advect a parcel to the bottom/top of atmosphere if the
+        temporal sampling of the data is coarse enough (e.g. hourly or 3 hourly). This option enables
+        the trajectory to capture large areas of rising/sinking air, in phenomena such as overturning
+        circulations. If incW if False, then compute back trajectories on a pressure level with no
+        vertical advection (essentially a laminar, stratified flow).
+    cyclic: book
+        If cyclic is True, then the code will assume no east/west boundary and if a back trajectory
+        reaches the boundary, it will emerge out of the other side. This option is useful for large
+        domains such as the K-SCALE tropical channel, where there are only north/south boundaries in
+        the domain.
+
+    Returns
+    -------
+    iris.cube.Cube
+        An iris cube of the age of air data, with 3 dimensions (time, latitude, longitude).
+
     """
-    # Filter cubelist and pull out required cubes (sometimes multiple with same name, need to check)
-    for cube in cubelist:
-        if cube.standard_name == "x_wind":
-            x_wind = cube
-        elif cube.standard_name == "y_wind":
-            y_wind = cube
-        elif cube.standard_name == "upward_air_velocity":
-            z_wind = cube
-        elif cube.standard_name == "geopotential_height":
-            geopot = cube
+    # multicore = None
 
-    # Check that all cubes are of same dimension (some might differ slightly, extra y point often)
-    if not x_wind.shape == y_wind.shape == z_wind.shape == geopot.shape:
-        quit("Cubes not same dimlen! Quitting...")
+    pass
 
-    # Get time units and assign for later
-    if str(x_wind.coord("time").units) == "hours since 1970-01-01 00:00:00":
-        timeunit = "hour"
-    else:
-        quit("Unsupported time base! Quitting...")
 
-    # Make data non-lazy to speed up code.
-    print("Making data non-lazy...")
-    x_arr = x_wind.data
-    y_arr = y_wind.data
-    z_arr = z_wind.data
-    g_arr = geopot.data
+#    # Filter cubelist and pull out required cubes (sometimes multiple with same name, need to check)
+#    for cube in cubelist:
+#        if cube.standard_name == "x_wind":
+#            x_wind = cube
+#        elif cube.standard_name == "y_wind":
+#            y_wind = cube
+#        elif cube.standard_name == "upward_air_velocity":
+#            z_wind = cube
+#        elif cube.standard_name == "geopotential_height":
+#            geopot = cube
 
-    # Smooth vertical velocity if using, to 2sigma (standard for 0.5 degree).
-    # TODO could this be a user input, or should it scale with resolution?
-    if includevertical:
-        print("Smoothing vertical velocity...")
-        for t in range(0, z_arr.shape[0]):
-            for p in range(0, z_arr.shape[1]):
-                z_arr[t, p, :, :] = gaussian_filter(
-                    z_arr[t, p, :, :], [2, 2], mode="nearest"
-                )
-    else:
-        z_arr[:] = 0
+#    # Check that all cubes are of same dimension (some might differ slightly, extra y point often)
+#    if not x_wind.shape == y_wind.shape == z_wind.shape == geopot.shape:
+#        quit("Cubes not same dimlen! Quitting...")
 
-    # Get time spacing of cube -
-    dt = x_wind.coord("time").points[1:] - x_wind.coord("time").points[:-1]
-    if np.all(dt == dt[0]):
-        dt = dt[0]
-    else:
-        quit("Time not monotonically increasing, not supported...")
+#    # Get time units and assign for later
+#    if str(x_wind.coord("time").units) == "hours since 1970-01-01 00:00:00":
+#        timeunit = "hour"
+#    else:
+#        quit("Unsupported time base! Quitting...")
 
-    # Get coord points
-    lats = x_wind.coord("latitude").points
-    lons = x_wind.coord("longitude").points
-    time = x_wind.coord("time").points
+#    # Make data non-lazy to speed up code.
+#    print("Making data non-lazy...")
+#    x_arr = x_wind.data
+#    y_arr = y_wind.data
+#    z_arr = z_wind.data
+#    g_arr = geopot.data
 
-    # Get array index for user specified pressure level.
-    try:
-        plev_idx = np.where(x_wind.coord("pressure").points == plev)[0][0]
-    except IndexError:
-        print("Can't find plev ", str(plev), " in ", x_wind.coord("pressure").points)
-        quit()
+#    # Smooth vertical velocity if using, to 2sigma (standard for 0.5 degree).
+#    # TODO could this be a user input, or should it scale with resolution?
+#    if includevertical:
+#        print("Smoothing vertical velocity...")
+#        for t in range(0, z_arr.shape[0]):
+#            for p in range(0, z_arr.shape[1]):
+#                z_arr[t, p, :, :] = gaussian_filter(
+#                    z_arr[t, p, :, :], [2, 2], mode="nearest"
+#                )
+#    else:
+#        z_arr[:] = 0
 
-    # Initialise cube containing age of air.
-    ageofair_cube = iris.cube.Cube(
-        np.zeros((len(time), len(lats), len(lons))),
-        long_name="age_of_air",
-        dim_coords_and_dims=[
-            (x_wind.coord("time"), 0),
-            (x_wind.coord("latitude"), 1),
-            (x_wind.coord("longitude"), 2),
-        ],
-    )
+#    # Get time spacing of cube -
+#    dt = x_wind.coord("time").points[1:] - x_wind.coord("time").points[:-1]
+#    if np.all(dt == dt[0]):
+#        dt = dt[0]
+#    else:
+#        quit("Time not monotonically increasing, not supported...")
 
-    print("STARTING AOA DIAG...")
-    start = datetime.datetime.now()
-    if multicore is not None:
-        # Multiprocessing on each longitude slice
-        # TODO: there was an error where 719 was passed to x idx.
-        pool = multiprocessing.Pool(multicore)
-        func = partial(
-            aoa_core,
-            np.copy(x_arr),
-            np.copy(y_arr),
-            np.copy(z_arr),
-            np.copy(g_arr),
-            lats,
-            lons,
-            dt,
-            plev_idx,
-            timeunit,
-            cyclic,
-            tmpdir,
-        )
-        pool.map(func, range(0, x_wind.shape[3]))
-    else:
-        # Single core - better for debugging.
-        for i in range(0, x_wind.shape[3]):
-            aoa_core(
-                x_arr,
-                y_arr,
-                z_arr,
-                g_arr,
-                lats,
-                lons,
-                dt,
-                plev_idx,
-                timeunit,
-                cyclic,
-                tmpdir,
-                i,
-            )
+#    # Get coord points
+#    lats = x_wind.coord("latitude").points
+#    lons = x_wind.coord("longitude").points
+#    time = x_wind.coord("time").points
 
-    # Verbose for time taken to run, and collate tmp ndarrays into final cube, and return
-    print("AOA DIAG DONE, took", (datetime.datetime.now() - start).total_seconds(), "s")
-    for i in range(0, x_wind.shape[3]):
-        ageofair_cube.data[:, :, i] = np.load(
-            tmpdir + "/aoa_frag_" + str(i).zfill(4) + ".npy"
-        )
-        os.remove(tmpdir + "/aoa_frag_" + str(i).zfill(4) + ".npy")
+#    # Get array index for user specified pressure level.
+#    try:
+#        plev_idx = np.where(x_wind.coord("pressure").points == plev)[0][0]
+#    except IndexError:
+#        print("Can't find plev ", str(plev), " in ", x_wind.coord("pressure").points)
+#        quit()
 
-    return ageofair_cube
+#    # Initialise cube containing age of air.
+#    ageofair_cube = iris.cube.Cube(
+#        np.zeros((len(time), len(lats), len(lons))),
+#        long_name="age_of_air",
+#        dim_coords_and_dims=[
+#            (x_wind.coord("time"), 0),
+#            (x_wind.coord("latitude"), 1),
+#            (x_wind.coord("longitude"), 2),
+#        ],
+#    )
+
+#    print("STARTING AOA DIAG...")
+#    start = datetime.datetime.now()
+#    if multicore is not None:
+#        # Multiprocessing on each longitude slice
+#        # TODO: there was an error where 719 was passed to x idx.
+#        pool = multiprocessing.Pool(multicore)
+#        func = partial(
+#            aoa_core,
+#            np.copy(x_arr),
+#            np.copy(y_arr),
+#            np.copy(z_arr),
+#            np.copy(g_arr),
+#            lats,
+#            lons,
+#            dt,
+#            plev_idx,
+#            timeunit,
+#            cyclic,
+#        )
+#        pool.map(func, range(0, x_wind.shape[3]))
+#    else:
+#        # Single core - better for debugging.
+#        for i in range(0, x_wind.shape[3]):
+#            aoa_core(
+#                x_arr,
+#                y_arr,
+#                z_arr,
+#                g_arr,
+#                lats,
+#                lons,
+#                dt,
+#                plev_idx,
+#                timeunit,
+#                cyclic,
+#                tmpdir,
+#                i,
+#            )
+
+#    # Verbose for time taken to run, and collate tmp ndarrays into final cube, and return
+#    print("AOA DIAG DONE, took", (datetime.datetime.now() - start).total_seconds(), "s")
+#    for i in range(0, x_wind.shape[3]):
+#        ageofair_cube.data[:, :, i] = np.load(
+#            tmpdir + "/aoa_frag_" + str(i).zfill(4) + ".npy"
+#        )
+#        os.remove(tmpdir + "/aoa_frag_" + str(i).zfill(4) + ".npy")
+
+#    return ageofair_cube
